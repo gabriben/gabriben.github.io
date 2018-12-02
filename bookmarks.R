0bce5651ea5605e79b7e3ff293daa8b191ea6220
Gabriel Bénédict <gbndict@gmail.com>
2018-12-02 14:21:04 -0500

    some docs

---
+++ bookmarks.R
@@ -0,0 +1,4 @@
+library(rjson)
+f <- fromJSON(file = "/Users/gabrielbenedict/Google_Drive/docs/UNIS/KU Leuven/Exchange/columbia/Courses/Applied Data Science/Projects/gabriben.github.io/bookmarks")
+
+f$roots$bookmark_bar$children
